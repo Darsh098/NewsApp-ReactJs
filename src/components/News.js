2fdf61905049b3da5f1251cf34b88313c3697b75
Darsh Doshi <darshdoshi01@gmail.com>
2024-02-28 13:17:22 +0530

    Added Style To NewsItem Component

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,20 @@ import NewsItem from './NewsItem';
 export class News extends Component {
     render() {
         return (
-            <div>
-                This Is A News Component
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-                <NewsItem />
-            </div>
+            <div className="container my-3">
+                <h2>NewsBlink - Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="MyTitle 1" description="My Description 1" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="MyTitle 2" description="My Description 2" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="MyTitle 3" description="My Description 3" />
+                    </div>
+                </div>
+            </div >
         )
     }
 }
