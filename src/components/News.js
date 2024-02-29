dc4110b8f2cef958c7e12286f9b3548dd8439e6e
Darsh Doshi <darshdoshi01@gmail.com>
2024-02-29 17:14:20 +0530

    Added Top Loading Bar

--- src/components/News.js
+++ src/components/News.js
@@ -34,16 +34,20 @@ export class News extends Component {
     }
 
     async updateNews(pageManipulaion) {
+        this.props.setProgress(10);
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category === "home" ? "general" : this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page + pageManipulaion}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json();
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false,
             page: this.state.page + pageManipulaion
         })
+        this.props.setProgress(100);
     }
 
     async componentDidMount() {
