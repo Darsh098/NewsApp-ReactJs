b2ea5839bcaa7502bb649cfc3f416dbeba4a4a36
Darsh Doshi <darshdoshi01@gmail.com>
2024-02-29 15:58:06 +0530

    Removed Next & Prev Button For Pagination And Added Infinite Scroll

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
 
@@ -22,7 +23,8 @@ export class News extends Component {
         this.state = {
             articles: [],
             page: 1,
-            loading: false
+            loading: false,
+            totalResults: 0
         }
         document.title = this.props.category === "home" ? "NewsBlink - Stay Informed, In a Blink!" : "NewsBlink - " + this.capitalizeFirstLetter(this.props.category);
     }
@@ -36,7 +38,6 @@ export class News extends Component {
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
@@ -49,42 +50,47 @@ export class News extends Component {
         this.updateNews(0);
     }
 
-    handleNextBtn = async () => {
-        if (this.state.page + 1 <= Math.ceil(this.state.totalResults / this.props.pageSize)) {
-            this.updateNews(1);
-        }
-    }
-    handlePrevBtn = async () => {
-        this.updateNews(-1);
+    fetchMoreData = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category === "home" ? "general" : this.props.category}&apiKey=8261cbfa5baf424b9c15ab5b99958959&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true });
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+            loading: false,
+            page: this.state.page + 1
+        })
     }
 
     render() {
         return (
-            <div className="container my-3">
+            <>
                 <h2 className="text-center">NewsBlink - {this.props.category === "home" ? "All Headlines" : this.capitalizeFirstLetter(this.props.category)}</h2>
-                {this.state.loading && < Spinner />}
-                <div className="row">
-                    {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title ? element.title : ""}
-                                description={element.description ? element.description.length >= 60 ? element.description.slice(0, 60) + "..." : element.description : ""}
-                                imageUrl={element.urlToImage ? element.urlToImage : "https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg?w=740"}
-                                newsUrl={element.url}
-                                author={element.author}
-                                publishedAt={element.publishedAt}
-                                source={element.source} />
+                {/* {this.state.loading && < Spinner />} */}
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className="container my-3">
+                        <div className="row">
+                            {this.state.articles.map((element) => {
+                                return <div className="col-md-4" key={element.url}>
+                                    <NewsItem title={element.title ? element.title : ""}
+                                        description={element.description ? element.description.length >= 60 ? element.description.slice(0, 60) + "..." : element.description : ""}
+                                        imageUrl={element.urlToImage ? element.urlToImage : "https://img.freepik.com/premium-vector/default-image-icon-vector-missing-picture-page-website-design-mobile-app-no-photo-available_87543-11093.jpg?w=740"}
+                                        newsUrl={element.url}
+                                        author={element.author}
+                                        publishedAt={element.publishedAt}
+                                        source={element.source} />
+                                </div>
+                            })}
                         </div>
-                    })}
-
-                </div>
-                <div className="container d-flex justify-content-between">
-                    <button type="button" disabled={this.state.page <= 1}
-                        className="btn btn-dark" onClick={this.handlePrevBtn}>{"< Previous"}</button>
-
-                    <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
-                        className="btn btn-dark" onClick={this.handleNextBtn}>{"Next >"}</button>
-                </div>
-            </div >
+                    </div>
+                </InfiniteScroll>
+            </ >
         )
     }
 }
